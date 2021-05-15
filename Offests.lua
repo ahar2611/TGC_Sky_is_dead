2ccf1eda377718ae91de32f357b1ddd4aa6f43b5
Kelrit402 <67088119+Kelrit402@users.noreply.github.com>
2021-05-15 13:56:25 +0900

    Offsets

---
+++ Offests.lua
@@ -0,0 +1,14 @@
+poffsets = {
+  wlevel = 0x00,
+  positX = 0x1A938,
+  positY = 0x1A93C,
+  positZ = 0x1A940,
+  bheight = 0x1A9FC,
+  pose = 0x1DC50,
+  pwing = 0x2036C,
+  ewing = 0x23068,
+  eprop = 0x23084,
+  famount = 0x24598,
+  gesture = 0x27864,
+  update = "21.05.15"
+  }
